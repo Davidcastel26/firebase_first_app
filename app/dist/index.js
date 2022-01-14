f3158c2bb3b18baccf7b86e795ca39fc056ecf0e
David Castellanos <davcastellanoslarios@gmail.com>
2022-01-14 14:57:57 -0600

    feat: importing initializeApp line 1 and 12 into:app/src/index.ts

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("firebase/app");
 const firebaseConfig = {
     apiKey: "AIzaSyDzh1AFlBV_Mc0wOHKhmk-yhYmoywrG_-A",
     authDomain: "fir-9-dojo-4f821.firebaseapp.com",
@@ -7,3 +9,4 @@ const firebaseConfig = {
     messagingSenderId: "31769837180",
     appId: "1:31769837180:web:25205a91ae5235a877d3f9"
 };
+(0, app_1.initializeApp)(firebaseConfig);
--- app/src/index.ts
+++ app/src/index.ts
@@ -1,3 +1,5 @@
+import { initializeApp } from 'firebase/app'
+
 const firebaseConfig = {
     apiKey: "AIzaSyDzh1AFlBV_Mc0wOHKhmk-yhYmoywrG_-A",
     authDomain: "fir-9-dojo-4f821.firebaseapp.com",
@@ -5,4 +7,6 @@ const firebaseConfig = {
     storageBucket: "fir-9-dojo-4f821.appspot.com",
     messagingSenderId: "31769837180",
     appId: "1:31769837180:web:25205a91ae5235a877d3f9"
-  };
+};
+
+initializeApp(firebaseConfig)
